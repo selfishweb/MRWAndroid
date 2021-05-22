175eb8ce6b432d66122d95879e119ddb188a2f5a
Alex <alejandro@selfish.com.mx>
2021-05-21 19:15:52 -0500

    Degradado de header y logo en top bar

--- app/src/main/java/mx/com/selfish/mrwandroid/MainActivity.kt
+++ app/src/main/java/mx/com/selfish/mrwandroid/MainActivity.kt
@@ -2,10 +2,20 @@ package mx.com.selfish.mrwandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        getSupportActionBar()?.setTitle("")
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true)
+        getSupportActionBar()?.setLogo(R.drawable.menu_logotipo)
+        getSupportActionBar()?.setDisplayUseLogoEnabled(true)
     }
+
 }
